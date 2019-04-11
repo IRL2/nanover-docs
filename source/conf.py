00718e1b25da9e0530d8a77473a039d4ad366f7d
Alex Binnie <alexjbinnie@gmail.com>
2019-04-11 11:59:50 +0100

    Set master_doc to index

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+
+master_doc = 'index'
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
