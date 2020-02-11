2930d18dd5b2a1cf015a2e798bfb789b465edf86
Jonathan Barnoud <jonathan@barnoud.net>
2020-02-11 12:08:22 +0000

    Debug on doc

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@ import sys
 import subprocess
 #sys.path.insert(0, os.path.abspath('../narupa-protocol/python-libraries/narupa-openmm'))
 
-subprocess.call('python ./install.py')
+subprocess.call('./install.py')
 
 # -- Project information -----------------------------------------------------
 
