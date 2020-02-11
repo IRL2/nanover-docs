aebd03270ab6c7564fa7d5d9a12cdaf9f66b85ac
Jonathan Barnoud <jonathan@barnoud.net>
2020-02-11 13:30:12 +0000

    Use conda packages

--- source/conf.py
+++ source/conf.py
@@ -15,8 +15,8 @@ import sys
 import subprocess
 #sys.path.insert(0, os.path.abspath('../narupa-protocol/python-libraries/narupa-openmm'))
 
-print('PATH', sys.path)
-subprocess.call('./install.py')
+#print('PATH', sys.path)
+#subprocess.call('./install.py')
 
 # -- Project information -----------------------------------------------------
 
