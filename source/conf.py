27ffd581fd3884521fb296c9ddf3ec8602e4181a
Rhoslyn Roebuck <rhoslynroebuck@gmail.com>
2024-09-02 17:30:22 +0200

    add copybutton
    Add package to requirements.txt & add to conf.py

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,7 @@ release = get_latest_release_version('IRL2', 'nanover-imd')
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.coverage', 'sphinx.ext.autosummary']
+              'sphinx.ext.coverage', 'sphinx.ext.autosummary', 'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
